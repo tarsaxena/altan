e58d22789774adf7cb2a267cb510074ed901f5f3
tarsaxena <trnsaxena9@gmail.com>
2020-06-01 04:07:32 -0700

    Update unique_number.php

--- exersize_1/3rd/unique_number.php
+++ exersize_1/3rd/unique_number.php
@@ -1,47 +1,22 @@
 <?php
 
-/*========================Using Linux==============*/
 
-#!/bin/bash
-# Syntax: $0 filename   
-# The input is assumed to be a .tsv file
-
-FILE="$1"
-
-cols=$(sed -n 1p $FILE | tr -cd '\t' | wc -c)
-cols=$((cols + 2 ))
-i=0
-for ((i=1; i < $cols; i++))
-do
-  echo Column $i ::
-  cut -f $i < "$FILE" | sort | uniq -c
-  echo
-done
-
-/*========================Using mysql==============*/
-
-CREATE TABLE `incr` (
-  `Id` int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (`Id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-Secondly create a stored procedure like this:
-
-DELIMITER ;;
-CREATE PROCEDURE dowhile()
-BEGIN
-  DECLARE v1 INT DEFAULT 5;
-  WHILE v1 > 0 DO
-    INSERT incr VALUES (NULL);
-    SET v1 = v1 - 1;
-  END WHILE;
-END;;
-DELIMITER ;
-Lastly call the SP:
-
-CALL dowhile();
-SELECT * FROM incr;
+/*=======================Unique Records=======================*/
+/*=================================================================*/
+$n = 100;  
+$numbers = range(0, $n-1);
+$rands = array();
+for ($i=0; $i < $n; $i++) {
+  $ok = false;
+  while (!$ok) {
+    $x = array_rand($numbers);
+    $ok = !in_array($numbers[$x], $rands) && $numbers[$x] != $i;
+  }
+  $rands[$i] = $numbers[$x];
+  unset($numbers[$x]);
+}
 
 
-
-
+echo "<pre/>";
+print_r($rands);
 ?>
